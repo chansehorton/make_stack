48eb8c3b7b4cfbad71750b8bd8eac6c80b9baa2f
CHANSE HORTON <chansehorton@yahoo.com>
2017-01-18 09:33:42 -0700

    two tests passing

--- stack.js
+++ stack.js
@@ -1,5 +1,21 @@
-function Stack(){
+'use strict'
 
+class Stack {
+  constructor(...args) {
+    this.length = 0
+
+    for (let i=0; i<args.length; i++) {
+      this[i] = args[i]
+      this.length += 1
+    }
+  }
+
+  empty() {
+    if (this.length === 0) {
+      return true
+    }
+    return false
+  }
 }
 
-module.exports = Stack;
+module.exports = Stack
